e4d98ed3ceabd9f578e15403656a811be77bcbde
Senula88 <senulananayakkara88@gmail.com>
2022-09-25 16:32:28 +0530

    Payment backend CRUD completed

--- ERPS/backend/controllers/payments.controller.js
+++ ERPS/backend/controllers/payments.controller.js
@@ -26,6 +26,76 @@ const addPayment = async (req, res) => {
     
   };
 
+  // Get all payments
+
+    const getPayments = async (req, res) => {
+
+    Payment.find().then((payments)=>{
+        res.json(payments)
+    }).catch((err)=>{
+        console.log(err);
+    })
+  
+  };
+
+  // Delete Payments
+
+ const deletePayment = async (req, res) => {
+    let paymentID  = req.params.id;
+  
+    const payment = await Payment.findOneAndDelete({ _id: paymentID })
+  
+    .then(() => {
+        res.status(200).send({status : "payment deleted"});    
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({status: "error with delete data"});
+    })
+  };
+
+ 
+  //Search for payment
+
+const getPayment = async (req, res) => {
+
+    let paymentId = req.params.id;
+    Payment.findById(paymentId)
+        .then((payment) => {
+            res.status(200).send({status: "Payment fetched", payment: payment})
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).send({status: "Error with get payment",error: err.message})
+        })
+   
+}
+
+// Update payment
+
+const updatePayment = async (req, res) => {
+    const { id } = req.params;
+
+  const payment = await Payment.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!payment) {
+    return res.status(400).json({ error: 'No such payment' });
+  } else {
+    return res.status(200).json(payment);
+  }
+
+}
+
+
+
+
   module.exports = {
-    addPayment
+    addPayment,
+    getPayments,
+    deletePayment,
+    getPayment,
+    updatePayment
   };
