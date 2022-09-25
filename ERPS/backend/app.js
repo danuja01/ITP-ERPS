7ffb01750e0bee694115ae22b68e26cda97aa75d
it21027888 <87113734+HiruniHerath@users.noreply.github.com>
2022-09-25 12:17:40 +0530

    Item crud function done.

--- ERPS/backend/app.js
+++ ERPS/backend/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const degreesRoutes = require('./routes/degrees.routes.js');
-const courseMaterialsRoutes = require('./routes/courseMaterials.routes.js');
+const foodsRoutes = require('./routes/fooditem.routes.js');
+
+//const courseMaterialsRoutes = require('./routes/courseMaterials.routes.js');
 
 require('dotenv').config();
 
@@ -14,7 +16,8 @@ app.use(express.json());
 
 //routes
 app.use('/api/degrees', degreesRoutes);
-app.use('/api/courseMaterials', courseMaterialsRoutes);
+app.use('/api/fooditem', foodsRoutes);
+//app.use('/api/courseMaterials', courseMaterialsRoutes);
 
 // connect to db
 mongoose
